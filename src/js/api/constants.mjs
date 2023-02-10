afdf87722ec5fee02cc1274a731510fe8424a3bc
martemoslet <martemoslet@gmail.com>
2023-02-10 15:00:55 +0100

    Adds image for missing images

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -2,3 +2,5 @@ export const API_HOST_URL = "https://api.noroff.dev";
 export const API_BASE = "/api/v1";
 export const API_AUCTION_BASE = "/auction";
 export const API_AUCTION_URL = `${API_HOST_URL}${API_BASE}${API_AUCTION_BASE}`;
+export const missingImg =
+  "https://cdn.pixabay.com/photo/2021/09/13/23/10/vinyl-6622596_960_720.jpg";
