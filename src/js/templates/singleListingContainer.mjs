c892161128a18e1abcc433326b7017c81db942fa
martemoslet <martemoslet@gmail.com>
2023-02-19 14:55:04 +0100

    Updates single listing

--- src/js/templates/singleListingContainer.mjs
+++ src/js/templates/singleListingContainer.mjs
@@ -12,11 +12,23 @@ export function singleListingContainer(listingData) {
      <div class="container col-12 col-md-7 col-lg-6 mt-3 mb-3">
      <div class="thumbnail">
          <div class="card">
-             <img class="" src="${listingData.media}" alt="${listingData.title}" onerror="this.onerror=null; this.src='${missingImg}'" />
+            <div class="card-header">${listingData.seller.name}</div>
+             <img class="" src="${listingData.media}" alt="${
+    listingData.title
+  }" onerror="this.onerror=null; this.src='${missingImg}'" />
              <div class="card-body">
                  <h2 class="card-title">${listingData.title}</h2>
                  <p class="card-text">${listingData.description}</p>
+                 
+                 
          </div>
      </div>
+     <div>
+     <p class="">Bids: ${listingData._count.bids}</p>
+     ${
+       listingData._count.bids > 0
+         ? `<p class="">Bidding price: ${listingData.bids[0].amount}</p>`
+         : ""
+     }</div>
  </div>`;
 }
