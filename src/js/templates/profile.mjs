5db1b1d467cb5aa95d6b1863fbfa64703df888a4
martemoslet <martemoslet@gmail.com>
2023-02-22 23:36:32 +0100

    Design updates

--- src/js/templates/profile.mjs
+++ src/js/templates/profile.mjs
@@ -14,13 +14,13 @@ export function userProfileTemplate(profile) {
         <div class="container">
             <div class="row">
                 <div class="col col-12 mb-3 pt-2">
-                    <h1 class="text-center pt-2 pb-4">Profile</h1>
                     <div class="card-body d-flex justify-content-center">
                     <img class="profile-img col-1 me-3" src="${profile.avatar}" alt="Profile image" onerror="this.onerror=null; this.src='${missingImg}'" />
-                    <div class="card-text">
+                    <div class="card-text profile-text">
                     <h2>${profile.name}</h2>
                     <p>${profile.email}</p>
                     <p>Credits: ${profile.credits}</p>
+                    <p>Listings: ${profile._count.listings}</p>
                     </div>
                  </div>
              </div>
