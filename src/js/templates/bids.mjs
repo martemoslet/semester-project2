8f9d9b434a2103d518138f46e46a5df60eb94cbc
martemoslet <martemoslet@gmail.com>
2023-02-26 22:33:19 +0100

    Updates design

--- src/js/templates/bids.mjs
+++ src/js/templates/bids.mjs
@@ -2,10 +2,17 @@ export function seeBids(listingData) {
   for (let i = 0; i < listingData.bids.length; i++) {
     const bidContainer = document.querySelector("#bid-container");
     bidContainer.innerHTML += `
-    <div class="col d-flex justify-content-center">
+    <div class="col text-center mt-2">
 
-    <p>${listingData.bids[i].bidderName} | ${listingData.bids[i].amount}</p>
+      <p class="bid-amount">${listingData.bids[i].amount}</p>
+      <p><strong>${listingData.bids[i].bidderName}</strong></p>
+
+    </div>
 
     `;
+  }
+  if (listingData.bids.length === 0) {
+    const bidContainer = document.querySelector("#bid-container");
+    bidContainer.innerHTML += `<p class="text-center">No bids yet!</p>`;
   }
 }
