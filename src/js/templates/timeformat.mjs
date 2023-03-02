0b272f4751a21839e1ea07cb879f6a73993bd339
martemoslet <martemoslet@gmail.com>
2023-03-02 13:35:59 +0100

    Deletes unused pages

--- src/js/templates/timeformat.mjs
+++
@@ -1,9 +0,0 @@
-export function dateFormat() {
-  for (let i = 0; i < listingData.length; i++) {
-    const convertDate = new Date(listingData[i].endsAt).toLocaleDateString(
-      "en-us",
-      { month: "short", day: "numeric" }
-    );
-  }
-  console.log();
-}
